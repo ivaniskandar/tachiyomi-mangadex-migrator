f7394ce084c00df2ef222719f928ebf72383cad0
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-05-08 23:09:01 +0700

    Release 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "xyz.ivaniskandar.ayunda"
         minSdk = 21
         targetSdk = 29
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
